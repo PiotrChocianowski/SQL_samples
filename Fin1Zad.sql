a269727a390a945258da6f2241b179705886c1f9
PiotrChocianowski <piotrchocianowski@gmail.com>
2025-05-23 22:55:15 +0200

    Fin1Zad.sql

--- Fin1Zad.sql
+++ Fin1Zad.sql
@@ -1,16 +1,16 @@
 /*jaki procent klientów ma daną liczbę zleceń*/
 select 
    orders1
-	,count(customer_id) as customers1
+	,count(customer_id) 				   as customers1
 	,total_customers
-	,round((count(customer_id)/total_customers)*100,2) as wynik
-	from 
+	,round((count(customer_id)/total_customers)*100,2) as 'wynik w %'
+from 
    ( 
-   select  
+select  
    customer_id
    ,count(order_id) as orders1
    ,(select count(distinct customer_id) from orders) as total_customers
-   from orders 
+from orders 
    group by 1
    ) o
    group by 1
