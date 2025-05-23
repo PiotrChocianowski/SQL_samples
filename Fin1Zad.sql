0e555f0a00bd6815140f88f8e7c58404616c84fb
PiotrChocianowski <piotrchocianowski@gmail.com>
2025-05-23 22:37:44 +0200

    Add files via upload

---
+++ Fin1Zad.sql
@@ -0,0 +1,22 @@
+/*jaki procent klientów ma daną liczbę zleceń*/
+select 
+   orders1
+	,count(customer_id) as customers1
+	,total_customers
+	,round((count(customer_id)/total_customers)*100,2) as wynik
+	from 
+   ( 
+   select  
+   customer_id
+   ,count(order_id) as orders1
+   ,(select count(distinct customer_id) from orders) as total_customers
+   from orders 
+   group by 1
+   ) o
+   group by 1
+   order by 1
+   
+   
+   
+   
+
